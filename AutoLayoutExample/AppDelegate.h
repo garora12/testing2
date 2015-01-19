181e60951b988120217e1e28c4d91dba0a1f5bd4
Gaurav Arora <gaurav@Gaurav-Arora.local>
2015-01-19 16:35:26 +0530

    3rd commit

--- AutoLayoutExample/AppDelegate.h
+++ AutoLayoutExample/AppDelegate.h
@@ -5,7 +5,7 @@
 //  Created by Gourav Arora on 08/01/15.
 //  Copyright (c) 2015 Gourav Arora. All rights reserved.
 //
-
+//  Copyright (c) 2015 Gourav Arora. All rights reserved.
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
